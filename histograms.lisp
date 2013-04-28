c4b1e49bfa07512e081e3b6532d89f2a23bda50b
Pranav Ravichandran <me@onloop.net>
2013-04-28 15:37:34 +0530

    Populatory commit.

---
+++ histograms.lisp
@@ -0,0 +1,23 @@
+(defpackage :histograms
+  (:use :cl))
+
+(in-package :histograms)
+
+(defun listMin (target)
+  ;; Returns the minimum element in a list.
+  (apply #'min target))
+
+(defun ratioList (target)
+  ;; Takes a list of numbers and returns the ratios between
+  ;; each and the smallest in the list.
+  (mapcar #'(lambda (each) (float (/ each (listMin target)))) target))
+
+(defun barCount (target)
+  ;; Takes a ratio list and counts the number of bars required
+  ;; for the histogram.
+  (mapcar #'(lambda (each) (round (/ each 0.5))) target))
+
+(defun generateBars (target)
+  ;; Takes a list of bar counts and generates bars (-).
+  (mapcar #'(lambda (each) (make-string each :initial-element #\-))
+	  target))
