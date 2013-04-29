b9d819155adb3b85ef076bd332b2afde94d29a95
Pranav Ravichandran <me@onloop.net>
2013-04-29 12:55:38 +0530

    Including modified changes from histUtil

--- histRenderer.lisp
+++ histRenderer.lisp
@@ -1,4 +1,5 @@
 (asdf:operate 'asdf:load-op :restas)
+(import :histograms)
 
 (restas:define-module :restas.histRenderer
     (:use :cl))
@@ -6,6 +7,14 @@
 (in-package :restas.histRenderer)
 
 (restas:define-route histInput (":(input)")
-  input)
+  (progn
+    (defparameter *histogramOutput*
+      (histograms::concatList
+       (histograms::generateBars
+	(histograms::barCount
+	 (histograms::ratioList
+	  (histograms::convertToListOfInts
+	   (histograms::stringSplit input #\-))))))))
+    *histogramOutput*)
 
-(restas:start :restas.histRenderer :port 8080)
+(restas:start :restas.histRenderer :port 8083)
