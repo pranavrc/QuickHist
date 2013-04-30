ada9206197cc2ff372e88afe1e9c82aef1205ca7
Pranav Ravichandran <me@onloop.net>
2013-04-30 07:33:00 +0530

    Exception handling.

--- histRenderer.lisp
+++ histRenderer.lisp
@@ -8,13 +8,16 @@
 
 (restas:define-route histInput (":(input)")
   (progn
-    (defparameter *histogramOutput*
-      (histograms::concatList
-       (histograms::generateBars
-	(histograms::barCount
-	 (histograms::ratioList
-	  (histograms::convertToListOfInts
-	   (histograms::stringSplit input #\-))))))))
+    (handler-case (progn (eval
+      (defparameter *histogramOutput*
+	(histograms::concatList
+	 (histograms::generateBars
+	  (histograms::barCount
+	   (histograms::ratioList
+	    (histograms::convertToListOfInts
+	     (histograms::stringSplit input #\,)))))))))
+      (error (e) (defparameter *histogramOutput* "Enter a valid set of numbers,
+separated by Commas. (/25,50,100,75 or /1.2,2.4,0.6 for instance.)"))))
     *histogramOutput*)
 
 (restas:define-route not-found ("*any")
