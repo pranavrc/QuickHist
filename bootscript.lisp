6365d8b019c121c3c637cb6f00e821609f9f1db4
Pranav Ravichandran <me@onloop.net>
2013-05-04 23:52:55 +0530

    Moved restas:start to boot script.

--- bootscript.lisp
+++ bootscript.lisp
@@ -3,4 +3,4 @@
 (ql:quickload :restas)
 
 (asdf:operate 'asdf:load-op '#:quickhist)
-(restas:start '#:restas.histRenderer)
+(restas:start '#:restas.histRenderer :hostname "quickhist.onloop.net" :port 8080)
